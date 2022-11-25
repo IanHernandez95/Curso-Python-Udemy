f37e7c9ce72ddb6f19f8464f9781759dcf33f185
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-11-25 18:55:13 -0300

    Dia 1 - Basicos de Python

---
+++ Dia-1/PrimerProyecto.py
@@ -0,0 +1,14 @@
+# print
+print('Hola Mundo')
+
+# String
+print('Texto')
+print('Texto\nDoslineas')
+print('Texto\tTabulado')
+print('Barra invertida: \\')
+
+# input
+print('Tu nombre es :' + input('tu nombre: ') + ' ' + input('tu apellido: '))
+
+# Tarea primer Dia
+print('La Cerveza se llama \n' + '"' + input('Animal Favorito: ') + ' ' + input('Color favorito: ') + '"')
