fe39aebd03e3e7885a1c879156ee43bc9db223a5
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-11-28 15:35:37 -0300

    Dia 2 - Variables, tipos de datos y operadores matematicos

---
+++ Dia-2/ProyectoDia2.py
@@ -0,0 +1,4 @@
+nombre = input('Cual es tu nombre: ')
+ventas = float(input('Cuanto vendiste este mes: '))
+
+print(f'{nombre} este mes tu ganancia es de {round(ventas*13/100,2)}')
---
+++ Dia-2/Variables.py
@@ -0,0 +1,100 @@
+# Tipos de Datos(
+    # String ( srt ) 'hola','123412'
+    # Integer ( int ) 152, 185415, 1 
+    # Floating ( Float ) 1.121, 0.4115, -0.12
+    # listas ( list ) ['sal',1,2,'marte']
+    # diccionarions ( dist ) {'color':'rojo, 'arte':'cine'}
+    # tuple ( tup ) ('lun','mar','mier')
+    # sets (set) {'a','b','c','d'}
+    # booleanos (bool) True,False
+# )
+
+# nombre = 'John'
+# edad = 30
+# edad2 = 15
+# print( nombre, edad + edad2 )
+
+# nombre = input('Dime tu nombre: ')
+# print('Tu nombre es ' + nombre)
+
+# Reglas de variables (
+#     1: legible
+#     2: unidad(minusculas y guiones bajos)
+#     3: Hispanismos (evitar acentos y Ñ)
+#     4: numeros( no al comienzo )
+#     5: evitar signos
+#     6: evitar palabras claves
+# )
+
+# mi_numero = 1 + 3
+# print(mi_numero)
+# print(type(mi_numero))
+# print(mi_numero + mi_numero)
+
+# mi_numero = 5 + 5.8
+# print(mi_numero)
+# print(type(mi_numero))
+# print(mi_numero + mi_numero)
+
+# edad = input('Dime tu edad: ')
+# print('Tu edad es ' + edad)
+# print(type(edad))
+# edad = int(edad)
+# nueva_edad = 1 + edad
+
+# print('Vas a cumplir ' + nueva_edad)
+
+# conversiones
+# num1 = 20
+# num2 = 30.5
+
+# num1 = num1 + num2
+
+# print(type(num1))
+# print(type(num2))
+
+# num1 = 5.8
+# print(num1)
+# print(type(num1))
+
+# num2 = int(num1)
+# print(num2)
+# print(type(num2))
+
+# x = 10 
+# y = 5 
+# print(f'Mis numeros son {x} y {y} es igual a {x + y}')
+
+# color = 'rojo'
+# matricula = 123434
+# print(f'El auto es {color} y su matricula es {matricula}')
+
+# OPERADORES
+
+# x = 6
+# y = 2
+# z = 7
+
+# print(f'{x} mas {y} es igual a {x + y}')
+# print(f'{x} menos {y} es igual a {x - y}')
+# print(f'{x} por {y} es igual a {x * y}')
+# print(f'{x} dividido {y} es igual a {x / y}')
+
+# Division al piso
+# print(f'{z} dividido al piso {y} es igual a {z // y}')
+# print(f'{z} modulo de {y} es igual a {z % y}')
+
+# Potencia
+# print(f'{x} potencia de {y} es igual a {x ** y}')
+# print(f'{x} potencia de {3} es igual a {x ** 3}')
+
+# raiz Cuadrada
+# print(f'La raiz cuadrada de {x} es {x**0.5}')
+
+# Redondeo
+# num1 = round(90/7)
+# print(num1)
+
+# valor = round(95.6666666666666)
+# print(valor)
+# print(type(valor))
